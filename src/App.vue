e66ccd71d80749c6cb1435e5b6d5723e7c3069f7
HelipengTony <tony.hlp@hotmail.com>
2020-08-14 18:07:04 +0800

    update app

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,49 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <NexmentContainer :config="configs" />
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import { Component, Vue } from "vue-property-decorator";
+import NexmentContainer from "nexment-vue";
+import "nexment-vue/dist/nexment-vue.css";
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
-export default class App extends Vue {}
+Vue.use(NexmentContainer);
+
+@Component
+export default class App extends Vue {
+  configs = {
+    pageKey: "vueDemoPage",
+    enableLinkInput: true,
+    leancloud: {
+      appId: "6Kcb9HB1iOR87HUbAvnMyUER-gzGzoHsz",
+      appKey: "zf3gAf9CGIHLL8Crctb0sJLV",
+      serverURL: "https://leancloud.ouorz.com",
+    },
+    admin: {
+      name: "TonyHe",
+      email: "he@holptech.com",
+    },
+  };
+}
 </script>
 
 <style lang="scss">
+body {
+  margin: 0px;
+}
+@media screen and (min-width: 1000px) {
+  #app {
+    width: 650px !important;
+    padding: 20px !important;
+    border-left: 1px solid #eee;
+    border-right: 1px solid #eee;
+  }
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  margin: 0 auto;
+  padding: 0px;
 }
 </style>
