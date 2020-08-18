f84e86bb219492b086ab4d286462d6b7de59e5df
HelipengTony <tony.hlp@hotmail.com>
2020-08-18 13:53:09 +0800

    update example

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,74 @@
 <template>
   <div id="app">
-    <NexmentContainer :config="configs" />
+    <div class="title-div">
+      <div class="div-1">
+        <img src="https://i.loli.net/2020/08/18/fzRDFIx7315GZbs.png" />
+        <h1>
+          Nexment-Vue
+        </h1>
+      </div>
+      <div>
+        <a href="https://nexment-demo.ouorz.com" target="_blank">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            width="26"
+            height="26"
+            class="svg-1"
+          >
+            <path fill="none" d="M0 0h24v24H0z"></path>
+            <path
+              d="M12 13.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm-.528 2.994c.175.21.351.414.528.609.177-.195.353-.398.528-.609a24.883 24.883 0 0 1-1.056 0zm-1.995-.125a20.678 20.678 0 0 1-2.285-.368c-.075.35-.132.69-.17 1.016-.19 1.583.075 2.545.478 2.777.403.233 1.368-.019 2.645-.974.263-.197.528-.416.794-.655a20.678 20.678 0 0 1-1.462-1.796zm7.331-.368c-.717.16-1.483.284-2.285.368a20.678 20.678 0 0 1-1.462 1.796c.266.24.531.458.794.655 1.277.955 2.242 1.207 2.645.974.403-.232.667-1.194.479-2.777a11.36 11.36 0 0 0-.17-1.016zm1.45-.387c.577 2.639.274 4.74-1.008 5.48-1.282.74-3.253-.048-5.25-1.867-1.997 1.819-3.968 2.606-5.25 1.866-1.282-.74-1.585-2.84-1.009-5.48C3.167 14.794 1.5 13.48 1.5 12s1.667-2.793 4.241-3.614c-.576-2.639-.273-4.74 1.009-5.48 1.282-.74 3.253.048 5.25 1.867 1.997-1.819 3.968-2.606 5.25-1.866 1.282.74 1.585 2.84 1.009 5.48C20.833 9.206 22.5 10.52 22.5 12s-1.667 2.793-4.241 3.614zm-7.32-9.779a11.36 11.36 0 0 0-.793-.655C8.868 4.225 7.903 3.973 7.5 4.206c-.403.232-.667 1.194-.479 2.777.04.327.096.666.17 1.016a20.678 20.678 0 0 1 2.286-.368c.475-.653.965-1.254 1.462-1.796zm3.585 1.796c.802.084 1.568.209 2.285.368.075-.35.132-.69.17-1.016.19-1.583-.075-2.545-.478-2.777-.403-.233-1.368.019-2.645.974a11.36 11.36 0 0 0-.794.655c.497.542.987 1.143 1.462 1.796zm-1.995-.125c-.175-.21-.351-.414-.528-.609-.177.195-.353.398-.528.609a24.884 24.884 0 0 1 1.056 0zm-4.156 7.198a24.884 24.884 0 0 1-.528-.914c-.095.257-.183.51-.263.761.257.056.521.107.79.153zm1.932.234a22.897 22.897 0 0 0 3.392 0A22.897 22.897 0 0 0 15.392 12a22.897 22.897 0 0 0-1.696-2.938 22.897 22.897 0 0 0-3.392 0A22.897 22.897 0 0 0 8.608 12a22.897 22.897 0 0 0 1.696 2.938zm5.852-4.728c.095-.257.183-.51.263-.761a17.974 17.974 0 0 0-.79-.153 24.884 24.884 0 0 1 .527.914zM6.13 9.837c-.34.11-.662.23-.964.36C3.701 10.825 3 11.535 3 12c0 .465.7 1.175 2.166 1.803.302.13.624.25.964.36.222-.7.497-1.426.825-2.163a20.678 20.678 0 0 1-.825-2.163zm1.45-.388c.081.25.169.504.264.76a24.884 24.884 0 0 1 .528-.913c-.27.046-.534.097-.791.153zm10.29 4.714c.34-.11.662-.23.964-.36C20.299 13.175 21 12.465 21 12c0-.465-.7-1.175-2.166-1.803a11.36 11.36 0 0 0-.964-.36c-.222.7-.497 1.426-.825 2.163.328.737.603 1.462.825 2.163zm-1.45.388c-.081-.25-.169-.504-.264-.76a24.884 24.884 0 0 1-.528.913c.27-.046.534-.097.791-.153z"
+              fill="rgba(52,72,94,1)"
+            ></path>
+          </svg> </a
+        ><a href="https://github.com/HelipengTony/nexment-vue" target="_blank">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            width="26"
+            height="26"
+            class="svg-2"
+          >
+            <path fill="none" d="M0 0h24v24H0z"></path>
+            <path
+              d="M12 2C6.475 2 2 6.475 2 12a9.994 9.994 0 0 0 6.838 9.488c.5.087.687-.213.687-.476 0-.237-.013-1.024-.013-1.862-2.512.463-3.162-.612-3.362-1.175-.113-.288-.6-1.175-1.025-1.413-.35-.187-.85-.65-.013-.662.788-.013 1.35.725 1.538 1.025.9 1.512 2.338 1.087 2.912.825.088-.65.35-1.087.638-1.337-2.225-.25-4.55-1.113-4.55-4.938 0-1.088.387-1.987 1.025-2.688-.1-.25-.45-1.275.1-2.65 0 0 .837-.262 2.75 1.026a9.28 9.28 0 0 1 2.5-.338c.85 0 1.7.112 2.5.337 1.912-1.3 2.75-1.024 2.75-1.024.55 1.375.2 2.4.1 2.65.637.7 1.025 1.587 1.025 2.687 0 3.838-2.337 4.688-4.562 4.938.362.312.675.912.675 1.85 0 1.337-.013 2.412-.013 2.75 0 .262.188.574.688.474A10.016 10.016 0 0 0 22 12c0-5.525-4.475-10-10-10z"
+              fill="rgba(52,72,94,1)"
+            ></path>
+          </svg>
+        </a>
+      </div>
+    </div>
+    <div class="switch-div">
+      <button
+        :class="'btn-left ' + (switchStatus ? 'btn-select' : '')"
+        @click="toggleSwitch"
+      >
+        Vue.js
+      </button>
+      <button
+        :class="'btn-right ' + (!switchStatus ? 'btn-select' : '')"
+        @click="toggleSwitch"
+      >
+        Web Component
+      </button>
+    </div>
+    <div class="nexment-div">
+      <div v-if="switchStatus">
+        <NexmentContainer :config="configs" />
+      </div>
+      <div v-if="!switchStatus">
+        <nexment-container
+          email="he@holptech.com"
+          name="TonyHe"
+          serverurl="https://leancloud.ouorz.com"
+          appkey="zf3gAf9CGIHLL8Crctb0sJLV"
+          appid="6Kcb9HB1iOR87HUbAvnMyUER-gzGzoHsz"
+          pagekey="vueDemoPage"
+          enablelinkinput="true"
+        ></nexment-container>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -13,6 +81,7 @@ Vue.use(NexmentContainer);
 
 @Component
 export default class App extends Vue {
+  switchStatus = true;
   configs = {
     pageKey: "vueDemoPage",
     enableLinkInput: true,
@@ -26,6 +95,9 @@ export default class App extends Vue {
       email: "he@holptech.com",
     },
   };
+  toggleSwitch() {
+    this.switchStatus = !this.switchStatus;
+  }
 }
 </script>
 
@@ -45,5 +117,85 @@ body {
   width: 100%;
   margin: 0 auto;
   padding: 0px;
+}
+
+.switch-div {
+  display: flex;
+  background: #f7f8f9;
+  padding: 1.1px 0.2px;
+  border-radius: 6px;
+  transition: ease-in-out 0.1s;
+  height: 33px;
+  margin: 5px 8px 20px 8px;
+}
+
+.btn-left {
+  flex: 1;
+  background: transparent;
+  border: none;
+  outline: none;
+  padding-left: 0px;
+  color: #999999;
+  padding-right: 0px;
+  transition: ease-in-out 0.1s;
+  cursor: pointer;
+}
+
+.btn-select {
+  box-shadow: 0px 0px 3px #e1e4e8;
+  padding: 5px 0px;
+  font-weight: 600;
+  font-family: sans-serif;
+  border-radius: 6px;
+  background: #fff !important;
+  color: #555555 !important;
+  transition: ease-in-out 0.1s;
+}
+
+.btn-right {
+  flex: 1;
+  background: transparent;
+  border: none;
+  outline: none;
+  color: #999999;
+  padding-left: 0px;
+  padding-right: 0px;
+  transition: ease-in-out 0.1s;
+  cursor: pointer;
+}
+
+.title-div {
+  display: flex;
+  margin-bottom: 8px;
+  a {
+    text-decoration: none !important;
+  }
+  .div-1 {
+    display: flex;
+    flex: 1;
+    img {
+      width: 50px;
+      height: 50px;
+    }
+    h1 {
+      margin: 0px;
+      font-size: 30px;
+      padding-top: 3px;
+      margin-left: 5px;
+    }
+  }
+  .svg-1 {
+    border-right: 1px solid #eee;
+    padding-right: 8px;
+    margin-right: 6px;
+  }
+  .svg-2 {
+    margin-top: 13px;
+    margin-right: 6px;
+  }
+}
+
+.nexment-div {
+  padding: 0px 8px;
 }
 </style>
